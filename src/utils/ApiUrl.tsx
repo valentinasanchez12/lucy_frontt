9c7d9c0997bd580705c5defbb692bfa35e7fe369
JuanMa Galvis <jgalvis@hispasec.com>
2025-02-19 19:33:11 -0500

    fix: se agrega el type para la url

--- src/utils/ApiUrl.tsx
+++ src/utils/ApiUrl.tsx
@@ -1,5 +1,5 @@
 // Detectar la URL base según el entorno
-export const API_BASE_URL =
+export const API_BASE_URL: string =
     import.meta.env.VITE_API_BASE_URL && import.meta.env.VITE_API_BASE_URL !== ''
         ? import.meta.env.VITE_API_BASE_URL
         : `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}`;
